9f80f0f56141939329ffc9513dfbbdddbd0a709b
micjohnson <micjohnson@gmail.com>
2013-01-11 09:05:08 -0800

    Making lookup return false on error. Making delete give up if lookup fails

--- WeedStorage.php
+++ WeedStorage.php
@@ -5,72 +5,78 @@ use InvalidArgumentException;
 
 class WeedStorage
 {
-	protected $weedPhp;
-	
-	public function __construct($weedPhp)
-	{
-		$this->weedPhp = $weedPhp;
-	}
-	
-	/**
-	 * return a random server
-	 * @param unknown $volumeId
-	 */
-	public function randomLookup($volumeId)
-	{
-		$lookup = $this->weedPhp->lookup($volumeId);
-		$lookup = json_decode($lookup, true);
-		$locs = $lookup['locations'];
-		$count = count($locs);
-		$rand = rand(0, $count-1);
-		return $locs[$rand]['publicUrl'];
-	}
+    protected $weedPhp;
 
-	public function store($entity)
-	{
-		$dataArray = $entity->getData();
-		if(!is_array($dataArray)) {
-		    $dataArray = array( $dataArray );
-		}
-		$count = count($dataArray);
-		$files = array();
-		foreach($dataArray as $data) {
-		    $files[] = $data;
-		}
-		$replication = $entity->getReplicationScheme();
-		$assignResponse = $this->weedPhp->assign($count, $replication);
-		$assignResponse = json_decode($assignResponse, true);
-		$volumeAddress = $assignResponse['publicUrl'];
-		$fid = $assignResponse['fid'];
-		$entity->setFileId($fid);
-		$response = $this->weedPhp->storeMultiple($volumeAddress, $fid, $files);
-		return $response;
-	}
+    public function __construct($weedPhp)
+    {
+        $this->weedPhp = $weedPhp;
+    }
 
-	public function retrieve($entity, $version)
-	{
-		$fileId = $entity->getFileId();
-		if($version !== 0) {
-		    if(!$entity->hasVersion($version)) {
-		        throw new InvalidArgumentException('That version does not exist');
-		    }
-		    $fileId .= '_' . $entity->getVersionOffset($version);
-		}
-		$volumeId = explode(',', $entity->getFileId());
-		$volumeId = $volumeId[0];
-		$serverAddress = $this->randomLookup($volumeId);
-		
-		return $this->weedPhp->retrieve($serverAddress, $fileId);
-	}
-	public function delete($entity)
-	{
-		$fileId = $entity->getFileId();
-	    $volumeId = explode(',', $entity->getFileId());
-		$volumeId = $volumeId[0];
-		$serverAddress = $this->randomLookup($volumeId);
-		foreach($entity->getVersions() as $offset=>$name) {
-		    $this->weedPhp->delete($serverAddress, $fileId);
-		    $fileId = $entity->getFileId() . '_' . ($offset+1);
-		}
-	}
+    /**
+     * return a random server
+     * @param unknown $volumeId
+     *
+     * TODO Throw proper exception
+     */
+    public function randomLookup($volumeId)
+    {
+        $lookup = $this->weedPhp->lookup($volumeId);
+        $lookup = json_decode($lookup, true);
+        if($lookup === false || array_key_exists("error", $lookup)) {
+            return false;
+        }
+        $locs = $lookup['locations'];
+        $count = count($locs);
+        $rand = rand(0, $count-1);
+        return $locs[$rand]['publicUrl'];
+    }
+
+    public function store($entity)
+    {
+        $dataArray = $entity->getData();
+        if(!is_array($dataArray)) {
+            $dataArray = array( $dataArray );
+        }
+        $count = count($dataArray);
+        $files = array();
+        foreach($dataArray as $data) {
+            $files[] = $data;
+        }
+        $replication = $entity->getReplicationScheme();
+        $assignResponse = $this->weedPhp->assign($count, $replication);
+        $assignResponse = json_decode($assignResponse, true);
+        $volumeAddress = $assignResponse['publicUrl'];
+        $fid = $assignResponse['fid'];
+        $entity->setFileId($fid);
+        $response = $this->weedPhp->storeMultiple($volumeAddress, $fid, $files);
+        return $response;
+    }
+
+    public function retrieve($entity, $version)
+    {
+        $fileId = $entity->getFileId();
+        if($version !== 0) {
+            if(!$entity->hasVersion($version)) {
+                throw new InvalidArgumentException('That version does not exist');
+            }
+            $fileId .= '_' . $entity->getVersionOffset($version);
+        }
+        $volumeId = explode(',', $entity->getFileId());
+        $volumeId = $volumeId[0];
+        $serverAddress = $this->randomLookup($volumeId);
+
+        return $this->weedPhp->retrieve($serverAddress, $fileId);
+    }
+    public function delete($entity)
+    {
+        $fileId = $entity->getFileId();
+        $volumeId = explode(',', $entity->getFileId());
+        $volumeId = $volumeId[0];
+        if($serverAddress = $this->randomLookup($volumeId)) {
+            foreach($entity->getVersions() as $offset=>$name) {
+                $this->weedPhp->delete($serverAddress, $fileId);
+                $fileId = $entity->getFileId() . '_' . ($offset+1);
+            }
+        }
+    }
 }
